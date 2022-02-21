38a3a40cdcafe8f07577c81539b78d99e55c2999
timmychien <chien871101@gmail.com>
2022-02-22 00:38:34 +0800

    update

--- nft-client/src/App.js
+++ nft-client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { render } from "react-dom";
+//import { render } from "react-dom";
 import { Helmet } from 'react-helmet'
 //import logo from './logo.svg';
 //import {getData}from './promoteData.js';
