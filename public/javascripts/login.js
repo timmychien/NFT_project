e0bf83aa84b1616678055af35ff891088461cd76
黃靖涵 <kanko@ip150-28.vpnip.cc.ntu.edu.tw>
2022-04-21 00:46:03 +0800

    [ching] temp

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -1,45 +1,44 @@
-'use strict';
+// 'use strict';
 
-//Problem: Hints are shown even when form is valid
-//Solution: Hide and show them at appropriate times
-var $password = $("#userpass");
+// //Problem: Hints are shown even when form is valid
+// //Solution: Hide and show them at appropriate times
+// var $password = $("#userpass");
 
-//�K�X���or����
-var eye = document.getElementById("eye");
-function hideShowPsw() {
-    if ($password.type == "password") {
-        $password.type = "text";
-        eye.className = "fa fa-eye-slash";
-    } else {
-        $password.type = "password";
-        eye.className = "fa fa-eye";
-    }
-}
+// //�K�X���or����
+// var eye = document.getElementById("eye");
+// function hideShowPsw() {
+//     if ($password.type == "password") {
+//         $password.type = "text";
+//         eye.className = "fa fa-eye-slash";
+//     } else {
+//         $password.type = "password";
+//         eye.className = "fa fa-eye";
+//     }
+// }
 
+// //Hide hints
+// $("form span#warn").hide();
 
-//Hide hints
-$("form span#warn").hide();
+// function isPasswordValid() {
+//     return $password.val().length >= 8;
+// }
 
-function isPasswordValid() {
-    return $password.val().length >= 8;
-}
+// function passwordEvent() {
+//     //Find out if password is valid
+//     if (isPasswordValid()) {
+//         //Hide hint if valid
+//         $password.next().hide();
+//     } else {
+//         //else show hint
+//         $password.next().show();
+//     }
+// }
 
-function passwordEvent() {
-    //Find out if password is valid  
-    if (isPasswordValid()) {
-        //Hide hint if valid
-        $password.next().hide();
-    } else {
-        //else show hint
-        $password.next().show();
-    }
-}
+// function enableSubmitEvent() {
+//     $("#submit").prop("disabled", !canSubmit());
+// }
 
-function enableSubmitEvent() {
-    $("#submit").prop("disabled", !canSubmit());
-}
+// //When event happens on password input
+// $password.focus(passwordEvent).keyup(passwordEvent).keyup(enableSubmitEvent);
 
-//When event happens on password input
-$password.focus(passwordEvent).keyup(passwordEvent).keyup(enableSubmitEvent);
-
-enableSubmitEvent();
+// enableSubmitEvent();
